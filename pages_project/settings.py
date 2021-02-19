4f3a48653e93c7e86a82015f8d9700765c1c735e
Aditi Memani <aditimemani@Aditis-MBP.fios-router.home>
2021-02-18 23:40:05 -0500

    New updates for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7num(7)ba-g_2$3dbj5-b40utysv&do*7thjsmfwg55!#t!^5^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
